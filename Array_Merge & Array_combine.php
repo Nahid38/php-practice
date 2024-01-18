0663dc7f28cb487a0f3d8647a2b56813bbcabe38
Nahid Hossen <nahid957hossen@gmail.com>
2024-01-18 21:42:28 +0600

    Multi assso array test

--- Array_Merge & Array_combine.php
+++ Array_Merge & Array_combine.php
@@ -1,7 +1,7 @@
 <?php
 $fruit=['a'=>"orange",'b'=>"banana",'c'=>"garps"];
 $veggie=['b'=>"carrot",'e'=>"pea"];
-$newArray=array_merge($fruit,$veggie);
+$newArray=array_merge_recursive($fruit,$veggie);
 
 echo "<pre>";
 print_r($newArray);
